e65917f0554dbf2c46e80f4b18f5cd1964d6ae9b
Dynia <piotr.dynia@gmail.com>
2023-10-17 17:04:36 +0200

    Fighting with js

--- main.go
+++ main.go
@@ -1,28 +1,76 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
-	"Participate/backend/controllers"
-	"Participate/backend/middlewares"
-	"Participate/backend/models"
-
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
-func main() {
-	models.ConnectDataBase()
+var logger = logrus.New()
+
+var logLevelMap = map[string]logrus.Level{
+	"trace": logrus.TraceLevel,
+	"debug": logrus.DebugLevel,
+	"info":  logrus.InfoLevel,
+	"warn":  logrus.WarnLevel,
+	"error": logrus.ErrorLevel,
+}
+
+type arguments struct {
+	LogLevel       string
+	BindAddress    string
+	BindPort       int
+	StaticContents string
+}
+
+func runServer(args arguments) error {
+	level, ok := logLevelMap[args.LogLevel]
+	if !ok {
+		return fmt.Errorf("invalid log level: %s", args.LogLevel)
+	}
+	logger.SetLevel(level)
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.WithFields(logrus.Fields{
+		"args": args,
+	}).Info("Given options")
 
 	r := gin.Default()
 
-	public := r.Group("/api")
+	r.Use(static.Serve("/", static.LocalFile(args.StaticContents, false)))
 
-	public.POST("/register", controllers.Register)
-	public.POST("/login", controllers.Login)
+	if err := r.Run(fmt.Sprintf("%s:%d", args.BindAddress, args.BindPort)); err != nil {
+		return err
+	}
 
-	protected := r.Group("/api/admin")
-	protected.Use(middlewares.JwtAuthMiddleware())
-	protected.GET("/customer", controllers.CurrentCustomer)
+	return nil
+}
+
+func main() {
+	args := arguments{
+		LogLevel:       "info",
+		BindAddress:    "0.0.0.0",
+		BindPort:       9080,
+		StaticContents: "./frontend/static",
+	}
+
+	if err := runServer(args); err != nil {
+		logger.WithError(err).Fatal("Server exits with error")
+	}
+	//models.ConnectDataBase()
+	//
+	//r := gin.Default()
+	//
+	//public := r.Group("/api")
+	//
+	//public.POST("/register", controllers.Register)
+	//public.POST("/login", controllers.Login)
+	//
+	//protected := r.Group("/api/admin")
+	//protected.Use(middlewares.JwtAuthMiddleware())
+	//protected.GET("/customer", controllers.CurrentCustomer)
 
 	// TODO To be deleted, left as an example
 	//r.GET("/users/:id", func(c *gin.Context) {
@@ -37,7 +85,7 @@ func main() {
 	//	c.JSON(200, user)
 	//})
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal(err)
-	} // listen and serve on 0.0.0.0:8080
+	//if err := r.Run(":8080"); err != nil {
+	//	log.Fatal(err)
+	//} // listen and serve on 0.0.0.0:8080
 }
