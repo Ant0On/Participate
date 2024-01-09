12dbd9b44bf88b580c892d4a46421d86075af8b9
Grzegorz Wal <101324337+s22042@users.noreply.github.com>
2024-01-09 19:24:08 +0100

    Signup screen (#15)
    * Signup screen
    
    * fix assets
    
    * font hotfix
    
    ---------
    
    Co-authored-by: Dynia <piotr.dynia@gmail.com>

--- frontend/src/components/login/SignUpSection.vue
+++ frontend/src/components/login/SignUpSection.vue
@@ -1,31 +1,31 @@
 <script setup>
-import {reactive, ref, defineEmits } from 'vue';
+import {reactive, ref} from 'vue';
 import * as Yup from 'yup';
 
 import TextInput from "@/components/ui/TextInput.vue";
-import LoginButtons from "@/components/login/LoginButtons.vue";
 import PasswordInput from "@/components/ui/PasswordInput.vue";
 import {useAuthStore} from "@/stores/auth.store";
+import LoginButton from "@/components/login/LoginButton.vue";
 
 
-const loginData = ref({
+const signUpData = ref({
+  Name: '',
   Login: '',
   Password: ''
 })
+
 const errors = reactive({
   apiError: ""
 })
 
 async function onSubmit() {
   const authStore = useAuthStore();
-  await schema.validate(loginData.value).then(() => {
-    return authStore.login(loginData.value.Login, loginData.value.Password)
-  }).catch(error =>{
-        if(error === "Bad Request")
-        {
-          errors.apiError = "Incorrect login data!"
-        }
-        else{
+  await schema.validate(signUpData.value).then(() => {
+    return authStore.signUp(signUpData.value.Name, signUpData.value.Login, signUpData.value.Password)
+  }).catch(error => {
+        if (error === "Bad Request") {
+          errors.apiError = "Incorrect sing up data!"
+        } else {
           errors.apiError = error.message
         }
       }
@@ -33,47 +33,48 @@ async function onSubmit() {
 }
 
 const schema = Yup.object().shape({
-  Login: Yup.string().required('Login is required'),
+  Name: Yup.string().required('Name is requred'),
+  Login: Yup.string().required('Email is required'),
   Password: Yup.string().when('Login', {
     is: (value) => value !== '' || typeof value !== 'undefined',
     then: schema => schema.required('Password is required')
   } )
 });
 
-function onSignUp(){
-  emits('signUpClicked')
-}
 
-const emits = defineEmits(['signUpClicked'])
 </script>
 
 <template>
-  <div class="login_section">
-    <p>Welcome back. Please log in to your account</p>
+  <div class="signup_section">
+    <p>Create new account</p>
     <div class="errors" v-if="errors.apiError">{{ errors.apiError }}</div>
-    <form class="login_form" @submit.prevent>
-      <TextInput width="440px" labelText="Your email" placeholder="Type your email" :isRequired="true" v-model="loginData.Login"/>
-      <PasswordInput v-model="loginData.Password" @keyup.enter="onSubmit"/>
-      <LoginButtons @log-in-clicked="onSubmit" @sign-up-clicked="onSignUp"/>
+    <form class="signup_form" @submit.prevent>
+      <TextInput width="440px" labelText="Your name" placeholder="Type your name" :isRequired="true"
+                 v-model="signUpData.Name"/>
+      <TextInput width="440px" labelText="Your email" placeholder="Type your email" :isRequired="true"
+                 v-model="signUpData.Login"/>
+      <PasswordInput v-model="signUpData.Password" @keyup.enter="onSubmit"/>
+      <LoginButton text="Sign up" @button-clicked="onSubmit" @keyup.enter="onSubmit"/>
     </form>
   </div>
 </template>
 
 <style scoped>
-div.login_section {
+div.signup_section {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   row-gap: 40px;
-  margin-left: 50px;
   margin-top: 50px;
+  margin-right: 8%;
 }
-div.errors{
+
+div.errors {
   font-family: "Sarabun", Helvetica;
 
 }
 
-.login_form {
+.signup_form {
   display: flex;
   flex-direction: column;
   row-gap: 40px;
@@ -90,7 +91,7 @@ p {
 }
 
 @media (max-width: 900px) {
-  div.login_section {
+  div.signup_section {
     margin-left: 5%;
   }
 }
--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,7 +1,14 @@
 <script setup>
+import {ref} from 'vue';
 import Footer from "@/components/layout/FooterBar.vue";
 import NavBar from "@/components/nav/NavBar.vue";
 import LoginSection from "@/components/login/LoginSection.vue";
+import SignUpSection from "@/components/login/SignUpSection.vue";
+
+const loginPage = ref(true)
+
+
+
 </script>
 
 <template>
@@ -9,8 +16,16 @@ import LoginSection from "@/components/login/LoginSection.vue";
   <div class="logging">
     <NavBar currentPage="login"/>
     <div class="login_container">
-      <LoginSection/>
-      <img alt="Image" src="../assets/img/login_image.png"/>
+      <Transition name="slide-fade">
+        <div class="login" v-if="loginPage">
+          <LoginSection @sign-up-clicked="loginPage = !loginPage"/>
+          <img alt="Image" src="../assets/img/login_image.png"/>
+        </div>
+        <div class="signup" v-else>
+          <img alt="Image" src="../assets/img/login_image.png"/>
+          <SignUpSection/>
+        </div>
+      </Transition>
     </div>
     <Footer/>
   </div>
@@ -23,13 +38,35 @@ import LoginSection from "@/components/login/LoginSection.vue";
   justify-content: space-between;
 }
 
-.login_container {
+.login {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   margin: 30px 9% 10px 9%;
   background: white;
 }
+.signup {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 30px 9% 10px 9%;
+  background: white;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.4s ease-in-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
 @media(max-width: 900px){
   img{
     display: none;
