3c273723ff7467ae5d6844fc0d472926abdff36b
grzegorz-wal <s22042@pjwstk.edu.pl>
2024-01-21 16:49:19 +0100

    Finished screen data account info

--- frontend/src/components/account/data/AccountData.vue
+++ frontend/src/components/account/data/AccountData.vue
@@ -1,18 +1,70 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, reactive} from 'vue';
+import * as Yup from 'yup';
 
 import {useAuthStore} from "@/stores/auth.store";
 import TextInput from "@/components/ui/TextInput.vue";
+import {fetchWrapper} from "@/_helpers/fetch-wrapper";
 
 const auth = useAuthStore();
 const user = auth.user;
 
 const userRole = user.Role
 
-function onSubmit() {
+const errors = reactive({
+  apiError: ""
+})
 
+const customerData = ref({
+  name: user.first_name,
+  lastName: user.last_name,
+  email: user.email,
+})
+
+const hostData = ref({
+  description: user.description ? user.description : '',
+  phoneNumber: user.phoneNumber ? user.phoneNumber : '',
+  bankAccount: user.bankAccount ? user.bankAccount : '',
+})
+
+async function onSubmit() {
+  await schemaCustomer.validate(customerData.value).then(async () => {
+    if(user.Role === "host"){
+      await schemaHost.validate(hostData.value).then(async () => {
+        await fetchWrapper.post(`/api/host/${user.ID}/change/description`, {
+          description: hostData.value.description
+        })
+        await fetchWrapper.post(`/api/host/${user.ID}/change/phone_number`, {
+          phone_number: hostData.value.phoneNumber
+        })
+        await fetchWrapper.post(`/api/host/${user.ID}/change/bank_account`, {
+          bank_account: hostData.value.bankAccount
+        })
+
+        user.description = hostData.value.description
+        user.phone_number = hostData.value.phone_number
+        user.bank_account = hostData.value.bank_account
+      })
+
+    }
+    isSubmitMode.value = false;
+    await fetchWrapper.post(`/api/customer/${user.ID}/change/first_name`, {
+      first_name: customerData.value.name.trim()
+    })
+    await fetchWrapper.post(`/api/customer/${user.ID}/change/last_name`, {
+      last_name: customerData.value.lastName.trim()
+    })
+    await fetchWrapper.post(`/api/customer/${user.ID}/change/email`,{
+      email: customerData.value.email.trim()
+    })
+    user.first_name = hostData.value.first_name
+    user.last_name = hostData.value.last_name
+    user.email = hostData.value.email
+  }).catch(error =>{
+        error.apiError = "Incorrect data!"
+      }
+  )
 }
-
 function onCancel() {
   isSubmitMode.value = false
 }
@@ -23,6 +75,17 @@ function onChangeData() {
 
 const isSubmitMode = ref(false)
 
+const schemaCustomer = Yup.object().shape({
+  name: Yup.string().min(5).required('Name is required'),
+  lastName: Yup.string().min(5).required('Last name is required'),
+  email: Yup.string().email().required('Email is required'),
+});
+
+const schemaHost = Yup.object().shape({
+  description: Yup.string().required('Offer type is required'),
+  phoneNumber: Yup.number().min(9),
+  bankAccount: Yup.number().min(16),
+});
 
 </script>
 
@@ -30,16 +93,16 @@ const isSubmitMode = ref(false)
   <div class="account_data_component">
     <div class="user_data">
       <div class="user_fields">
+        <div class="errors" v-if="errors.apiError">{{ errors.apiError }}</div>
         <div class="customer_fields">
-          <TextInput label-text="Name" :is-active="isSubmitMode" :model-value="user.first_name" width="100%"/>
-          <TextInput label-text="Last Name" :is-active="isSubmitMode" :model-value="user.last_name" width="100%"/>
-          <TextInput label-text="Email" :is-active="isSubmitMode" :model-value="user.email" width="100%"/>
+          <TextInput label-text="Name" :is-active="isSubmitMode" v-model="customerData.name" width="100%"/>
+          <TextInput label-text="Last Name" :is-active="isSubmitMode" v-model="customerData.lastName" width="100%"/>
+          <TextInput label-text="Email" :is-active="isSubmitMode" v-model="customerData.email" width="100%"/>
         </div>
-        <div class="host_fields" >
-<!--        <div class="host_fields" v-if="userRole === 'host'">-->
-          <TextInput label-text="Description" :is-active="isSubmitMode" :model-value="user.email" width="100%"/>
-          <TextInput label-text="Phone number" :is-active="isSubmitMode" :model-value="user.email" width="100%"/>
-          <TextInput label-text="Bank account" :is-active="isSubmitMode" :model-value="user.email" width="100%"/>
+        <div class="host_fields" v-if="userRole === 'host'">
+          <TextInput label-text="Description" :is-active="isSubmitMode" v-model="hostData.description" width="100%"/>
+          <TextInput label-text="Phone number" :is-active="isSubmitMode" v-model="hostData.phoneNumber" width="100%"/>
+          <TextInput label-text="Bank account" :is-active="isSubmitMode" v-model="hostData.bankAccount" width="100%"/>
         </div>
       </div>
       <div class="buttons">
@@ -134,5 +197,7 @@ div.buttons{
 .button_cancel {
   background-color: rgba(255, 0, 0, 60%);
 }
-
+div.errors{
+  font-family: "Sarabun", Helvetica;
+}
 </style>
