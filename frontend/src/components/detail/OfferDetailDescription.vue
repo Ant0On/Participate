a2ec6fe052b970d8f92fffb5d92324ba46111ee0
Grzegorz Wal <101324337+s22042@users.noreply.github.com>
2024-02-11 00:10:10 +0100

    Offer description added (#31)

--- frontend/src/components/detail/OfferDetailDescription.vue
+++ frontend/src/components/detail/OfferDetailDescription.vue
@@ -3,6 +3,7 @@ import {defineProps, defineEmits} from 'vue';
 import OfferDetailHeader from "@/components/detail/OfferDetailHeader.vue";
 import OfferDetailReserve from "@/components/detail/OfferDetailReserve.vue";
 import OfferDetailHost from "@/components/detail/OfferDetailHost.vue";
+import OfferDetailInformations from "@/components/detail/OfferDetailInformation.vue";
 
 const props = defineProps({
   name: '',
@@ -11,6 +12,7 @@ const props = defineProps({
   numberOfPeople: 0,
   hostId: 0,
   type: '',
+  description: '',
 })
 const emits = defineEmits(['moveToSummary'])
 
@@ -21,6 +23,8 @@ const emits = defineEmits(['moveToSummary'])
     <OfferDetailHeader :name="name" :location="location" :type="type" :numberOfPeople="numberOfPeople"/>
     <div class="line"/>
     <OfferDetailReserve @move-to-summary="$emit('moveToSummary')"/>
+    <div class="line"/>
+    <OfferDetailInformations :description="description"/>
     <div class="line"/>
     <OfferDetailHost />
   </div>
