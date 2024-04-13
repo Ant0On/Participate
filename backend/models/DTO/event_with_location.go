a6504cc1c4917d375e9c9ab7137a4ca37f63b4b1
Antoni Cieslak <acieslak@grupawp.pl>
2024-04-13 19:17:29 +0200

    Small files refactor

--- backend/models/DTO/event_with_location.go
+++
@@ -1,9 +0,0 @@
-package DTO
-
-import "backend/models"
-
-type EventWithLocation struct {
-	OfferWithLocation
-	Price float64          `json:"price" binding:"required,gt=0"`
-	Type  models.EventType `json:"type" binding:"required,oneof=conference concert festival 'sports event'"`
-}
