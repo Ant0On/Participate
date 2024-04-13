9b4af96d07a0d08735c19fdd5e17e76fa68ec21d
Antoni Cieślak <101286863+Ant0On@users.noreply.github.com>
2024-04-13 19:18:08 +0200

    Small files refactor (#50)
    Co-authored-by: Antoni Cieslak <acieslak@grupawp.pl>

--- backend/models/DTO/offer_with_location.go
+++ backend/models/DTO/offer_with_location.go
@@ -1,5 +1,11 @@
 package DTO
 
+import (
+	"time"
+
+	"backend/models"
+)
+
 type OfferWithLocation struct {
 	OfferID       uint    `json:"offer_id" binding:"required"`
 	Title         string  `json:"title" binding:"required,min=2"`
@@ -10,4 +16,26 @@ type OfferWithLocation struct {
 	CountryName   string  `json:"country_name" binding:"required,min=3"`
 	UserID        uint    `json:"user_id" binding:"required"`
 	Discount      float64 `json:"discount" binding:"required,min=0,max=100"`
+}
+
+type EventWithLocation struct {
+	OfferWithLocation
+	Price float64          `json:"price" binding:"required,gt=0"`
+	Type  models.EventType `json:"type" binding:"required,oneof=conference concert festival 'sports event'"`
+}
+
+type ActivityWithLocation struct {
+	OfferWithLocation
+	Price    float64             `json:"price" binding:"required,gt=0"`
+	Skill    models.SkillLevel   `json:"skill_level" binding:"required,oneof=beginner intermediate advanced"`
+	Type     models.ActivityType `json:"type" binding:"required,oneof=indoor outdoor"`
+	Duration time.Duration       `json:"duration" binding:"required"`
+}
+
+type AccommodationWithLocation struct {
+	OfferWithLocation
+	PricePerDay      float64                  `json:"price_per_day" binding:"required,gt=0"`
+	IsAnimalFriendly bool                     `json:"is_animal_friendly"`
+	Type             models.AccommodationType `json:"type" binding:"required,oneof=hotel hostel apartment villa guesthouse"`
+	Rating           int                      `json:"rating" binding:"required,min=1,max=5"`
 }
