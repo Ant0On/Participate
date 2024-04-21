0b737c1d5f6b6215b297cb1a6fe54695f6e3c493
grzegorz-wal <s22042@pjwstk.edu.pl>
2024-04-21 13:44:28 +0200

    feat: delete primevue and add vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,20 @@ import { createPinia } from 'pinia'
 import { router } from '@/router'
 import App from "./App.vue";
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(App)
 const pinia = createPinia()
 
+app.use(vuetify)
 app.use(pinia)
 app.use(router)
 
