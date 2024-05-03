875eef93e137caeda71efb019b3567551b2c43fa
grzegorz-wal <s22042@pjwstk.edu.pl>
2024-05-03 20:23:29 +0200

    feat: added form of payment

--- frontend/src/views/OfferDetailsPage.vue
+++ frontend/src/views/OfferDetailsPage.vue
@@ -11,10 +11,6 @@ import RoomDetail from "@/components/detail/RoomDetail.vue";
 const userStore = useAuthStore();
 const {user: user} = storeToRefs(userStore)
 
-const reserve = () => {
-  console.log("reserve")
-}
-
 const props = defineProps({
   type: String,
   id: String,
@@ -22,8 +18,15 @@ const props = defineProps({
 })
 const priceAfterDiscount = computed(() => calculatePriceAfterDiscount(offer.price, offer?.discount))
 
+const formFilled = ref(false)
 const cardPage = ref('description')
 const offerPage = ref('description')
+const chosenPayment = ref(null)
+const paymentType = ref([
+  {name: 'paypal', url: 'https://paypal.com', img: '@/assets/img/paypal.png'},
+  {name: 'credit_card', url: 'https://www.przelewy24.pl', img: '@/assets/img/credit_card.png'},
+  {name: 'bitcoin', url: 'https://bitcoin.org/', img: '@/assets/img/bitcoin.png'},
+])
 
 const typeLink = {
   'event': 'events',
@@ -288,7 +291,7 @@ onMounted(async () => {
                   <v-icon icon="mdi-information"></v-icon>
                 </v-btn>
                 <v-btn @click="cardPage = 'accommodation'">
-                  <!--              <v-btn @click="cardPage = 'accommodation'" v-if="type === 'accommodation'">-->
+                  <!--                <v-btn @click="cardPage = 'accommodation'" v-if="type === 'accommodation' && offer.type in ['hotel', 'hostel', 'guesthouse']">-->
                   <v-icon icon="mdi-home"></v-icon>
                 </v-btn>
                 <v-btn @click="cardPage = 'activity'">
@@ -394,9 +397,9 @@ onMounted(async () => {
                 Rooms
               </v-card-title>
               <v-list style="overflow: hidden">
-                    <v-list-item v-for="room in offer.rooms" :key="room.roomNumber">
-                      <RoomDetail :room="room"/>
-                    </v-list-item>
+                <v-list-item v-for="room in offer.rooms" :key="room.roomNumber">
+                  <RoomDetail :room="room"/>
+                </v-list-item>
               </v-list>
             </v-card-text>
 
@@ -447,23 +450,62 @@ onMounted(async () => {
       </v-window-item>
       <v-window-item value="summary">
         <v-card class="d-flex flex-row rounded-xl" :height="600">
-          <v-form>
-            <v-input></v-input>
-          </v-form>
-
-
-          <v-card-actions class="align-self-end w-100">
-            <v-btn
-                color="grey-darken-2 flex-grow"
-                text="Back"
-                block
-                border
-                @click="offerPage = 'description'"
-            ></v-btn>
-          </v-card-actions>
+          <v-card class="w-50 d-flex flex-column">
+            <v-form>
+<!--
+-->
+              <v-input></v-input>
+            </v-form>
+            <v-card-actions class="align-self-end w-100">
+              <v-btn
+                  color="grey-darken-2 flex-grow"
+                  text="Back"
+                  block
+                  border
+                  @click="offerPage = 'description'"
+              ></v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-fade-transition>
+            <v-card class="w-50 d-flex flex-column justify-space-between">
+<!--            <v-card class="w-50 d-flex flex-column" v-if="formFilled">-->
+              <v-card-title class="text-center">
+                Choose form of payment
+              </v-card-title>
+              <v-card-text class="mt-4">
+                <v-list @update:selected="(x) => console.log(x)">
+                  <v-list-item value="paypal">
+                    <template v-slot:prepend>
+                      <v-img :src="require('@/assets/img/paypal.png')" :height="75" :width="75"></v-img>
+                    </template>
+                    Paypal
+                  </v-list-item>
+                  <v-list-item value="credit_card">
+                    <template v-slot:prepend>
+                      <v-img :src="require('@/assets/img/credit_card.png')" :height="75" :width="75"></v-img>
+                    </template>
+                    Credit card
+                  </v-list-item>
+                  <v-list-item value="bitcoin">
+                    <template v-slot:prepend>
+                      <v-img :src="require('@/assets/img/bitcoin.png')" :height="75" :width="75"></v-img>
+                    </template>
+                    Bitcoin
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+              <v-card-actions class="w-100">
+                <v-btn
+                    color="blue-darken-2 flex-grow"
+                    text="Make reservation"
+                    block
+                    border
+                    @click="offerPage = 'description'"
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-fade-transition>
         </v-card>
-
-
       </v-window-item>
     </v-window>
 
